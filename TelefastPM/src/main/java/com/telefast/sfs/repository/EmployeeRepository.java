cd37e9f073136d730f725e9b41a1162e79b137b2
avinilcodes <avinilbedarkar@gmail.com>
2019-07-13 15:34:45 +0530

    Repository and Model

---
+++ TelefastPM/src/main/java/com/telefast/sfs/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.telefast.sfs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.telefast.sfs.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
